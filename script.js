7a550da5cacaa1b94d64452a549d5fc38de65e85
zelak-fabio <zelak.fabio@escola.pr.gov.br>
2025-04-02 12:25:53 +0000

    js do botao acessibildade mostra e contrai

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
@@ -14,6 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
-    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })    
 
 })
